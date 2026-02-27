1dee6008d627964b2437c712c015e4a300361a9b
CosmoLucs <cosmolucs@gmail.com>
2026-02-27 20:28:40 -0300

    chore: fix initial commit formmating issue

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Hub Inteligente de Recursos Educacionais API")
 
+
 @app.get("/health")
 def health_check():
     # Rota de health check exigida nos requisitos bônus do desafio!
